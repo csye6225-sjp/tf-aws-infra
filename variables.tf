7e8121896f2428ef9ad06b687e90cf95b9f2d36d
Sahana Jayadeva Prakash <jayadevaprakash.s@northeastern.edu>
2025-02-24 16:37:09 -0500

    checking formatter (#3)
    * checking formatter
    
    * fails
    
    * valid

--- variables.tf
+++ variables.tf
@@ -32,3 +32,21 @@ variable "private_subnet_cidrs" {
   description = "CIDR blocks for private subnets"
   type        = list(string)
 }
+
+variable "app_port" {
+  description = "The port on which the application runs"
+  type        = number
+  default     = 8080
+}
+
+variable "custom_ami" {
+  description = "Custom AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "aws_key_name" {
+  description = "AWS key pair name to associate with the instance (optional)"
+  type        = string
+  default     = ""
+}
+
